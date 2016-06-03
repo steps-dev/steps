97f9754b5f19b56441162b41fc88c47c47e2baf7
skiptoniam <skiptoniam@gmail.com>
2016-06-03 10:40:31 +1000

    tweak to hs_to_metapop

--- R/hs_to_metapop.R
+++ R/hs_to_metapop.R
@@ -31,8 +31,9 @@
 #' r2 <- raster(r)
 #' res(r2) <- 0.01
 #' r2 <- resample(r, r2)
-#' metapop_spatial <- hsm_to_metapop(r2,occ_thresh=0.8,x, distance=1000, p4s='+init=epsg:3577', 
-#'                                   givedist=TRUE, a=6,b=3,c=0.5,cc_mod='exp')
+#' proj4string(r2) <- '+init=epsg:4283'
+#' metapop_spatial <- hsm_to_metapop(r2,occ_thresh=0.8, distance=1000, p4s='+init=epsg:3577', 
+#'                                   givedist=TRUE, a=30,b=2,c=0.5,cc_mod='exp')
 hsm_to_metapop <- function(hsm, occ_thresh=0.8, distance, p4s, 
                            givedist=TRUE, a=6,b=3,c=0.5,cc_mod='exp'){
   rthr <- hsm > stats::quantile(hsm[], occ_thresh)
