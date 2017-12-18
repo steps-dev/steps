7d7376e4a8cd8ffd72669ec4add2368767f7c6e4
skiptoniam <skiptoniam@gmail.com>
2017-12-18 12:11:10 +1100

    moved non-finished functions into /building_r_functions

--- building_r_function/demographic_dynamics-class.R
+++ building_r_function/demographic_dynamics-class.R
@@ -15,7 +15,7 @@
 #' colnames(mat) <- rownames(mat) <- c('larvae','juvenile','adult') 
 #' demog <- as.demography(mat)
 #' n_stages <- length(states(demog))
-#' 
+
 alter_adult_survival <- function(demography, stage='adult', survial=0.9){
                                 #first check that demography is a demographic class
                                 if(!is.demography(demography))stop('check that demography is a demography object')
@@ -23,23 +23,8 @@ alter_adult_survival <- function(demography, stage='adult', survial=0.9){
                                 idx <-which(stages(demography)==stage)
                                 demography$stage_matrix[idx,idx]
                                   
-    
- 
- 
  }
-#' ##Define a function for manipulating habitat.
-#' ##This can be a custom function for manipulating rasters or existing functions. 
-#' ##Create a named list with corresponding parameters and values
-#' params = list(habitat,
-#'              fire_start_location = sample(ncell(habitat_suitability(habitat)),10),
-#'              prob = 0.24,
-#'              continue_to_burn_prob = 0.01)
-#'               
-#'## check habitat_suitability_dynamics produces expected output
-#' fire_affected_habitat_suitability <- as.habitat_dynamics(fire_module,params,check=TRUE)                   
-#'
-#'## If it does? create the habitat_dynamics (yay!).
-#'fire_habitat_suitability_dynamics <- as.habitat_dynamics(fun,params) 
+
 
 library(raster)
 library(dhmpr)
@@ -49,32 +34,6 @@ colnames(mat) <- rownames(mat) <- c('larvae','juvenile','adult')
 demog <- as.demography(mat)
 n_stages <- length(states(demog))
 print(demog)
-
-#set up starting populations
-set.seed(42)
-xy <- expand.grid(x=seq(145, 150, 0.1), y=seq(-40, -35, 0.1))
-Dd <- as.matrix(dist(xy))
-w <- exp(-1/nrow(xy) * Dd)
-Ww <- chol(w)
-xy$z <- t(Ww) %*% rnorm(nrow(xy), 0, 0.1)
-coordinates(xy) <- ~x+y
-r <- rasterize(xy, raster(points2grid(xy)), 'z')
-proj4string(r) <- '+init=epsg:4283'
-r[] <- scales::rescale(r[],to=c(0,1))
-random_populations <- sampleRandom(r, size=50, na.rm=TRUE, sp=TRUE)
-random_populations@data <- as.data.frame(t(rmultinom(50, size = 50, prob = c(0.8,0.2,0.1))))
-
-#set up habitat
-features <- list('habitat_suitability_map'=as.habitat_suitability(r),
-                 'population'=as.populations(random_populations),
-                 'carrying_capacity'=as.carrying_capacity(100))
-habitat <- as.habitat(features)
-print(habitat)
-
-## just playing with idea of make cell specific demography matricies which could be altered.
-pop_vec <- lapply(populations(habitat),function(x)c(x[]))
-pop_mat <- do.call(cbind,pop_vec)
-habsuit <- habitat_suitability(habitat)
 
 survival <- calc(habsuit,function(x){x[x<0.5] <- runif(1,0,.5) ;return(x)})
 c(adult_survival[])
--- tests/testthat/test_transition_class.R
+++ tests/testthat/test_transition_class.R
@@ -1,19 +1,19 @@
-context('transition-class')
+context('demography-class')
 
-test_that('transition classes work', {
+test_that('demography classes work', {
   
-  # the types of transition
+  # the types of demography
   mat <- matrix(c(.53,0,.42,0.1,0.77,0,0,0.12,0.9),nrow = 3,ncol = 3,byrow = TRUE)
-  tmat <- as.transition(mat)
+  tmat <- as.demography(mat)
   
-  # check as.transition won't handle a silly function
-  expect_error(as.transition(mat[,-1]))
-  expect_error(as.transition(mat[-1,]))
+  # check as.demography won't handle a silly function
+  expect_error(as.demography(mat[,-1]))
+  expect_error(as.demography(mat[-1,]))
   
   # check they have the right class
-  expect_s3_class(tmat, 'transition')
+  expect_s3_class(tmat, 'demography')
   
-  # check is.transition works on transitions
-  expect_true(is.transition(tmat))
+  # check is.demography works on demographys
+  expect_true(is.demography(tmat))
   
 })
