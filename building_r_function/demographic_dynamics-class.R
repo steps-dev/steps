391ad04c8d21fd82e8ca92d41ca2b919fb3615cc
skiptoniam <skiptoniam@gmail.com>
2017-12-18 15:49:15 +1100

    comment demo_dynamics

--- building_r_function/demographic_dynamics-class.R
+++ building_r_function/demographic_dynamics-class.R
@@ -25,34 +25,34 @@ alter_adult_survival <- function(demography, stage='adult', survial=0.9){
                                   
  }
 
-
-library(raster)
-library(dhmpr)
-# set a demography matrix
-mat <- matrix(c(.53,0,.62,0.15,0.87,0,0,0.12,0.9),nrow = 3,ncol = 3,byrow = TRUE)
-colnames(mat) <- rownames(mat) <- c('larvae','juvenile','adult') 
-demog <- as.demography(mat)
-n_stages <- length(states(demog))
-print(demog)
-
-survival <- calc(habsuit,function(x){x[x<0.5] <- runif(1,0,.5) ;return(x)})
-c(adult_survival[])
-
-all_cells_stage_matricies <- matrix(rep(c(demog$stage_matrix),npops),npops,length(c(demog$stage_matrix)),byrow = TRUE)
-
-stages_all <- 1:9
-stages_lar <- 1:3
-stages_juv <- 4:6
-stages_adl <- 7:9
-
-# survival prob affects which stage?
-# let's try with adult
-all_cells_stage_matricies[,stages_adl]<-c(survival[])*all_cells_stage_matricies[,stages_adl]
-
-pop_mat_new <- t(sapply(1:npops,function(i)pop_mat[i,]%*%matrix(all_cells_stage_matricies[i,],dim(demog$stage_matrix)[1],dim(demog$stage_matrix)[2])))
-
-r <- habitat_suitability(habitat)
-pops_updated <- lapply(split(pop_mat_new, rep(1:ncol(pop_mat_new), each = nrow(pop_mat_new))),function(x){r[]<-x;return(r)})
-
-# looks like adult pops are going down in less suitable areas - maholla.
-plot(stack(c(pops_updated,habsuit)))
+# playing with code that will manipulate the underlying stage matrices. 
+# library(raster)
+# library(dhmpr)
+# # set a demography matrix
+# mat <- matrix(c(.53,0,.62,0.15,0.87,0,0,0.12,0.9),nrow = 3,ncol = 3,byrow = TRUE)
+# colnames(mat) <- rownames(mat) <- c('larvae','juvenile','adult') 
+# demog <- as.demography(mat)
+# n_stages <- length(states(demog))
+# print(demog)
+# 
+# survival <- calc(habsuit,function(x){x[x<0.5] <- runif(1,0,.5) ;return(x)})
+# c(adult_survival[])
+# 
+# all_cells_stage_matricies <- matrix(rep(c(demog$stage_matrix),npops),npops,length(c(demog$stage_matrix)),byrow = TRUE)
+# 
+# stages_all <- 1:9
+# stages_lar <- 1:3
+# stages_juv <- 4:6
+# stages_adl <- 7:9
+# 
+# # survival prob affects which stage?
+# # let's try with adult
+# all_cells_stage_matricies[,stages_adl]<-c(survival[])*all_cells_stage_matricies[,stages_adl]
+# 
+# pop_mat_new <- t(sapply(1:npops,function(i)pop_mat[i,]%*%matrix(all_cells_stage_matricies[i,],dim(demog$stage_matrix)[1],dim(demog$stage_matrix)[2])))
+# 
+# r <- habitat_suitability(habitat)
+# pops_updated <- lapply(split(pop_mat_new, rep(1:ncol(pop_mat_new), each = nrow(pop_mat_new))),function(x){r[]<-x;return(r)})
+# 
+# # looks like adult pops are going down in less suitable areas - maholla.
+# plot(stack(c(pops_updated,habsuit)))
