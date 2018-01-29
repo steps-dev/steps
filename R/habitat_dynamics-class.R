254c162c8cfede89793598b3fe28bd6bf38f38c8
Casey Visintin <cvisintin@student.unimelb.edu.au>
2018-01-29 11:34:54 +1100

    changed run_habitat_dynamics to accept a habitat_dynamic object, a habitat object, and a timestep parameter

--- R/habitat_dynamics-class.R
+++ R/habitat_dynamics-class.R
@@ -28,13 +28,26 @@ is.habitat_dynamics <- function (x) inherits(x, 'habitat_dynamics')
 #' @name run_habitat_dynamics
 #' @export
 #' @description this bad boy will run the habitat_dynamics in a experiment.
-run_habitat_dynamics <- function(habitat_dynamics, ...){
+run_habitat_dynamics <- function(habitat_dynamics, habitat_object, time_step, ...){
   if(!is.habitat_dynamics(habitat_dynamics))
   stop("you need to define a habitat_dynamics module in order to run it within an experiment - see the documents for details")
   fun <- habitat_dynamics[[1]]
-  params <- habitat_dynamics[[2]]
+  params <- list(habitat_object, habitat_dynamics[[2]][[time_step]])
   altered_habitat <- do.call(fun,params)
   attr(altered_habitat, "habitat") <- "habitat"
   return(altered_habitat)
 }  
-  
+
+# #' @rdname habitat_dynamics
+# #' @name run_habitat_dynamics
+# #' @export
+# #' @description this bad boy will run the habitat_dynamics in a experiment.
+# run_habitat_dynamics <- function(habitat_dynamics, ...){
+#   if(!is.habitat_dynamics(habitat_dynamics))
+#     stop("you need to define a habitat_dynamics module in order to run it within an experiment - see the documents for details")
+#   fun <- habitat_dynamics[[1]]
+#   params <- habitat_dynamics[[2]]
+#   altered_habitat <- do.call(fun,params)
+#   attr(altered_habitat, "habitat") <- "habitat"
+#   return(altered_habitat)
+# } 
