192871d77e6dcdb7be1822943ccec94100893e7c
Casey Visintin <cvisintin@student.unimelb.edu.au>
2018-03-09 13:23:34 +1100

    updated test

--- tests/testthat/test_dispersal_functions.R
+++ tests/testthat/test_dispersal_functions.R
@@ -46,7 +46,15 @@ test_that('dispersal functions work', {
     barrier_type=1
   )
   
-  params2 <- list(
+  param2 <- list(
+    dispersal_distance=list('Stage_0-1'=0,'Stage_1-2'=10,'Stage_2-3'=10,'Stage_3+'=0),
+    dispersal_kernel=list('Stage_0-1'=0,'Stage_1-2'=exp(-c(0:9)^1/3.36),'Stage_2-3'=exp(-c(0:9)^1/3.36),'Stage_3+'=0),
+    dispersal_proportion=list('Stage_0-1'=0,'Stage_1-2'=0.35,'Stage_2-3'=0.35*0.714,'Stage_3+'=0),
+    barriers_map=disp.bar,
+    use_barriers=TRUE
+  )
+  
+  params3 <- list(
     dispersal_distance=list('Stage_0-1'=0,'Stage_1-2'=10,'Stage_2-3'=10,'Stage_3+'=0),
     dispersal_kernel=list('Stage_0-1'=0,'Stage_1-2'=exp(-c(0:9)^1/3.36),'Stage_2-3'=exp(-c(0:9)^1/3.36),'Stage_3+'=0),
     dispersal_proportion=list('Stage_0-1'=0,'Stage_1-2'=0.35,'Stage_2-3'=0.35*0.714,'Stage_3+'=0),
@@ -57,5 +65,6 @@ test_that('dispersal functions work', {
 
   expect_true(inherits(dispersal_core_ca(params, pop=pop, hsm=hab.suit, cc=hab.k), 'RasterStack'))
   expect_true(inherits(dispersal_core_ca(params2, pop=pop, hsm=hab.suit, cc=hab.k), 'RasterStack'))
+  expect_true(inherits(dispersal_core_ca(params3, pop=pop, hsm=hab.suit, cc=hab.k), 'RasterStack'))
 
 })
